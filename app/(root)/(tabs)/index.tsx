92ce287de4dc6f3a2e7ac718514711dc361432b7
Ajasa Shamsudeen <shamsudeenajasa@gmail.com>
2025-04-17 19:40:43 +0100

    add FeaturedCard and Card components for displaying property listings; implement Search component with debounced input for filtering results

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,20 +1,45 @@
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Search from "@/components/Search";
+import { Card, FeaturedCard } from "@/components/Cards";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-lg my-10 font-rubik text-3xl">Welcome to ReState</Text>
-      <Link href="/sign-in">SignIn</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">profile</Link>
-      <Link href="/properties/1">Property</Link>
-    </View>
+      <SafeAreaView className="bg-white h-full">
+        <View className="px-5">
+     
+          <View className="flex flex-row items-center justify-between mt-5">
+        <View className="flex flex-row items-center">
+              <Image
+                source={images.avatar}
+                className="size-12 rounded-full"
+                />
+                <View className="flex flex-col items-start ml-2 justify-center">
+                  <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+                  <Text className="text-base font-rubik-medium text-black-300">Shams.Dev</Text>
+                </View> 
+                </View>
+                <Image
+          source={icons.bell}
+          className="size-6"/>
+        </View>  
+        <Search/> 
+        <View className="my-5" >
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300 ">Featured</Text>
+            <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-ellipsis
+                text-primary-300">See All</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <FeaturedCard/>
+        <Card/>
+        </View>
+     
+      </SafeAreaView>
   );
 }
--- constants/images.ts
+++ constants/images.ts
@@ -1,5 +1,5 @@
 import onboarding from "@/assets/images/onboarding.png";
-import avatar from "@/assets/images/avatar.png";
+import avatar from "@/assets/images/avatar.jpg";
 import newYork from "@/assets/images/new-york.png";
 import japan from "@/assets/images/japan.png";
 import cardGradient from "@/assets/images/card-gradient.png";
